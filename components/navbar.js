3ebe43244c7471a484c5aa57273364b7c2f6124a
CameronJWalker <cameron.jr.walker@icloud.com>
2023-07-03 21:29:07 -0700

    minor css changes and added login/signup

--- components/navbar.js
+++ components/navbar.js
@@ -4,10 +4,13 @@ import Nav from '../styles/Nav.module.css'
 export default function Navbar() {
     return (
         <div className={Nav.bar}>
-            <Link href="/" className={Nav.homebutton}><h3>MyFitnessVault</h3></Link>
-            <Link href="/newworkout" className={Nav.homebutton}><p>Workout</p></Link>
-            <Link href="/nutrition" className={Nav.homebutton}><p>Nutrition</p></Link>
-            <Link href="/prlog" className={Nav.homebutton}><p>PR</p></Link>
+            <div className={Nav.title}>
+                <Link href="/" className={Nav.homebutton}><h3>MyFitnessVault</h3></Link>
+            </div>
+                <Link href="/newworkout" className={Nav.homebutton}><p>Workout</p></Link>
+                <Link href="/nutrition" className={Nav.homebutton}><p>Nutrition</p></Link>
+                <Link href="/prlog" className={Nav.homebutton}><p>PR</p></Link>            
+                <Link href="/userpage" className={Nav.homebutton}><p>Login</p></Link>
         </div>
     )
 }
---
+++ pages/userpage.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react"
+import User from "../styles/User.module.css"
+import { createClient } from "@supabase/supabase-js";
+
+export default function UserPage() {
+    
+    const [signUp, setSignUp] = useState({
+        username: "",
+        password: ""
+    })
+    const [login, setLogin] = useState({
+        username: "",
+        password: ""
+    })
+    const [loginMessage, setLoginMessage] = useState()
+
+    const [signUpMessage, setSignUpMessage] = useState()
+    
+    async function HandleSignupSubmit(e){ 
+        e.preventDefault()
+        }
+    async function HandleLoginSubmit(e){
+        e.preventDefault()
+    }
+    return (
+        <div className={User.container}>
+            <div className="logo">
+                <img src="2logo.png" alt="TheWorkoutApp" className={User.logo}/>
+            </div>
+            <div className={User.panel}>
+                <div className={User.signup}>
+                    <h2>SignUp</h2>
+                    <form onSubmit={HandleSignupSubmit} className={User.signup}>
+                        <label>Username</label>
+                        <input required type="text" placeholder="Enter Username" onChange={e => {setSignUp({...signUp,username: e.target.value })}}/>
+                        <label>Password</label>
+                        <input required type="password" placeholder="Enter Password" onChange={e => {setSignUp({...signUp,password: e.target.value })}}/>
+                        {signUpMessage ? <p id="ErrorMessage">{signUpMessage}</p> : <></>}
+                        <div className={User.button}>
+                            <input required type="submit" className={User.submit}/>
+                        </div>
+                    </form>
+                </div>
+
+                <div className={User.line}></div>
+
+                <div className={User.login}>
+                    <h2>Login</h2>
+                    <form onSubmit={HandleLoginSubmit}>
+                        <label>Username</label>
+                        <input required type="text" placeholder="Enter Username" onChange={e => {setLogin({...login,username: e.target.value })}}/>
+                        <label>Password</label>
+                        <input required type="password" placeholder="Enter Password" onChange={e => {setLogin({...login,password: e.target.value })}}/>
+                        {loginMessage ? <p id="ErrorMessage">{loginMessage}</p> : <></>}
+                        <div className={User.button}>
+                            <input required type="submit" className={User.submit}/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
